6e0ef67060c98614161276bcc1a37473b38af6e4
Quentin <contact@yovach.dev>
2023-10-12 22:57:58 +0200

    add "benchmark" with dynamic values

---
+++ common/multiple-times-dynamic/cookie.js
@@ -0,0 +1,32 @@
+{
+  console.time("split normal without spread");
+  for (let i = 0; i < 50_000; i++) {
+    const hostname = `_gh_sess=IAMASESSIONCOOKIE_${i}; path=/; secure; HttpOnly; SameSite=Lax`;
+    const host = hostname.split(";")[0];
+  }
+  console.timeEnd("split normal without spread");
+}
+{
+  console.time("split normal with spread");
+  for (let i = 0; i < 50_000; i++) {
+    const hostname = `_gh_sess=IAMASESSIONCOOKIE_${i}; path=/; secure; HttpOnly; SameSite=Lax`;
+    const [host] = hostname.split(";");
+  }
+  console.timeEnd("split normal with spread");
+}
+{
+  console.time("split with limit without spread");
+  for (let i = 0; i < 50_000; i++) {
+    const hostname = `_gh_sess=IAMASESSIONCOOKIE_${i}; path=/; secure; HttpOnly; SameSite=Lax`;
+    const host = hostname.split(";", 1)[0];
+  }
+  console.timeEnd("split with limit without spread");
+}
+{
+  console.time("split with limit with spread");
+  for (let i = 0; i < 50_000; i++) {
+    const hostname = `_gh_sess=IAMASESSIONCOOKIE_${i}; path=/; secure; HttpOnly; SameSite=Lax`;
+    const [host] = hostname.split(";", 1);
+  }
+  console.timeEnd("split with limit with spread");
+}
